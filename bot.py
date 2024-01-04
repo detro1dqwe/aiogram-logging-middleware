aa405f8bf3e74bc67f4928131d97e4dccbd1dc04
detro <81087734+detro1dqwe@users.noreply.github.com>
2024-01-04 19:59:41 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,16 @@
+import asyncio
+from aiogram import Bot, Dispatcher
 
+import middlewares
+
+async def main():
+    bot = Bot(config.bot_token.get_secret_value(), parse_mode="MarkdownV2")
+    dp = Dispatcher()
+    setup_logging()
+    
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+    
+    
+if name == "main":
+    asyncio.run(main())
